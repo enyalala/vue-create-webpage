25cf71a6d405213a3631d753c59db72c5d2a86b1
enyahuang <enya@enyahuangdeMacBook-Pro.local>
2022-10-12 19:00:54 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from "vue-router";
+import TopSwiper from "./components/TopSwiper.vue";
+import SecSwiper from "./components/SecSwiper.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="navbar">
+      <div class="navbar_content">
+        <ul class="left">
+          <li>
+            <a href="">
+              <img
+                class="navbar_logo"
+                src="./assets/img_kktv_logo.svg"
+                alt="logo"
+            /></a>
+          </li>
+          <li><a class="navbar_link" href="">精選</a></li>
+          <li><a class="navbar_link" href="">戲劇</a></li>
+          <li><a class="navbar_link" href="">動畫</a></li>
+          <li><a class="navbar_link" href="">娛樂</a></li>
+          <li><a class="navbar_link" href="">更多</a></li>
+          <li><a class="navbar_link" ßhref="">我的收藏</a></li>
+        </ul>
+      </div>
+    </div>
+    <div>
+      <TopSwiper />
+    </div>
+    <div class="swiper2">
+      <p class="title_text">新劇跟播中</p>
+      <SecSwiper />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  margin: 0px auto;
+  background-color: #222;
+}
+.navbar {
+  width: 100%;
+  color: #fff;
+  background-color: #551127;
+  z-index: 1000;
+  top: 0;
+}
+.navbar_content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+
+.navbar_logo {
+  width: 100px;
+  height: 22px;
+  vertical-align: middle;
+}
+.navbar .left {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.navbar_link {
+  margin: 0 10px;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  font-size: 14px;
+  text-decoration: none;
+  height: 56px;
+  line-height: 20px;
+  color: #fff;
+}
+ul {
+  list-style: none;
+}
+li {
+  list-style: none;
+}
+.swiper2 {
+  margin: 80px 0;
+}
+
+.title_text {
+  color: white;
+  font-size: 22px;
+}
+</style>
---
+++ src/components/TopSwiper.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "../assets/swiper.css";
+
+// import required modules
+import { Autoplay, Pagination, Mousewheel, Keyboard } from "swiper";
+
+const modules: any = [Autoplay, Pagination, Mousewheel, Keyboard];
+</script>
+
+<template>
+  <swiper
+    :spaceBetween="30"
+    :centeredSlides="true"
+    :autoplay="{
+      delay: 5000,
+      disableOnInteraction: false,
+    }"
+    :pagination="{
+      clickable: true,
+    }"
+    :navigation="true"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide
+      ><a href=""><img src="../assets/s1photo1.jpg" alt="photo" /></a
+    ></swiper-slide>
+    <swiper-slide
+      ><a href=""><img src="../assets/s1photo2.jpg" alt="photo" /></a
+    ></swiper-slide>
+    <swiper-slide
+      ><a href=""><img src="../assets/s1photo3.jpg" alt="photo" /></a
+    ></swiper-slide>
+    <swiper-slide
+      ><a href=""><img src="../assets/s1photo4.jpg" alt="photo" /></a
+    ></swiper-slide>
+    <swiper-slide
+      ><a href=""><img src="../assets/s1photo5.jpg" alt="photo" /></a
+    ></swiper-slide>
+  </swiper>
+</template>
