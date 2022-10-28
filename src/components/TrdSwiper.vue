979fd8901961ee02a6341e4ecd32670b1388af6a
enyahuang <enya@enyahuangdeMacBook-Pro.local>
2022-10-28 14:14:55 +0800

    Trd swiper v0

--- src/components/TrdSwiper.vue
+++ src/components/TrdSwiper.vue
@@ -9,17 +9,26 @@ import 'swiper/css/pagination'
 
 const modules3: any = [Pagination, Navigation]
 const slideMove = ref(false)
-
+const dramaHover = ref(true)
 const { dramaList } = useDramaInfo()
+
 const dramaId: number[] = reactive([])
+const sidephotoCount: number[] = reactive([])
 dramaList.forEach((drama) => {
   if (drama.classification.includes('熱播')) {
     dramaId.push(drama.dramaid)
+    sidephotoCount.push(drama.sidephotocount)
   }
 })
 
 const dramaIdDeleteLast = dramaId.slice(0, -1)
 const dramaIdLast = dramaId[dramaId.length - 1]
+// const sidephotoCountDeleteLast = sidephotoCount.slice(0, -1)
+// const sidephotoCountLast = sidephotoCount[sidephotoCount.length - 1]
+// const numDeleteLast = new Array(sidephotoCountDeleteLast.length)
+// const numCount = [...numDeleteLast.keys()]
+// numDeleteLast.fill(1)
+// const numLast = ref(1)
 
 const getS3ImageUrl = (name: number) => {
   return new URL(`../assets/img/dramacover/cover${name}.jpg`, import.meta.url)
@@ -31,6 +40,38 @@ const lastSlideShow = () => {
 }
 const lastSlideHide = () => {
   slideMove.value = false
+}
+
+const dramaover = () => {
+  dramaHover.value = false
+}
+
+const dramaleave = () => {
+  dramaHover.value = true
+}
+
+// 圖片自動切換 //////////////////////////////////////////////////////
+// setInterval(() => {
+//   numCount.forEach((count) => {
+//     if (numDeleteLast[count] < sidephotoCountDeleteLast[count]) {
+//       numDeleteLast[count]++
+//     } else {
+//       numDeleteLast[count] = 0
+//       numDeleteLast[count]++
+//     }
+//     return numDeleteLast[count]
+//   })
+// }, 5000)
+// console.log(dramaIdDeleteLast.indexOf(0))
+// setInterval(
+//   () => console.log(numDeleteLast[dramaIdDeleteLast.indexOf(0)]),
+//   5000
+// )
+const getSideUrl = (name: number, count: number) => {
+  return new URL(
+    `../assets/img/sidephoto/sidephoto${name}_${count}.jpg`,
+    import.meta.url
+  ).href
 }
 </script>
 
@@ -50,9 +91,15 @@ const lastSlideHide = () => {
       v-for="id in dramaIdDeleteLast"
       :key="id"
       :class="{ move: slideMove }"
-      ><router-link :to="'/dramalist/' + id"
+      ><router-link v-if="dramaHover" :to="'/dramalist/' + id"
         ><img
+          @mouseover="dramaover"
           :src="getS3ImageUrl(id)"
+          alt="photo" /></router-link
+      ><router-link v-else :to="'/dramalist/' + id"
+        ><img
+          @mouseleave="dramaleave"
+          :src="getSideUrl(id, 1)"
           alt="photo" /></router-link></swiper-slide
     ><swiper-slide
       @mouseover="lastSlideShow"
